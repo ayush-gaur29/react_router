3e17e4c7fc67bc64aea052309c0d3f81aad4eb6b
Siya Parashari <gaurayush264@gmail.com>
2026-06-12 16:02:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 
 const app = express();
-const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/donors", require("./routes/donorRoutes"));
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
 
-    app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
-      console.log("Server started");
+    const PORT = process.env.PORT || 8080;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
     });
 
   })
@@ -19,21 +32,3 @@ mongoose.connect(process.env.MONGO_URI)
     console.error("MongoDB error:", err);
     process.exit(1);
   });
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/donors", require("./routes/donorRoutes"));
-app.use("/api/auth", require("./routes/authRoutes"));
-
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend is running successfully");
-});
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
