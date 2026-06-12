00a65d86d720640268ad773cc5222f4882f2461c
Siya Parashari <gaurayush264@gmail.com>
2026-06-12 16:08:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => {
   console.log("MongoDB connected");
 
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT;
 
   app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port", PORT);
