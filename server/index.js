d411960c34a08d8229eabe927091d64b3a5e9988
Siya Parashari <gaurayush264@gmail.com>
2026-06-12 15:55:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,8 +24,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running successfully");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
